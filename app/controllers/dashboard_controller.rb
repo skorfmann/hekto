6788b65c55d5babe8f6c2403b3b1f55573ce775c
Sebastian Korfmann <sebastian@korfmann.net>
2024-10-10 13:31:08 +0200

    wip

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,9 @@
 class DashboardController < ApplicationController
   def show
-    @document_count = Document.count
-    @vendor_count = Vendor.count
-    @transaction_count = Transaction.count
-    @recent_documents = Document.order(created_at: :desc).limit(5)
+    @account_name = current_account.name
+    @document_count = current_account.documents.count
+    @vendor_count = current_account.vendors.count
+    @transaction_count = current_account.transactions.count
+    @recent_documents = current_account.documents.order(created_at: :desc).limit(5)
   end
 end
