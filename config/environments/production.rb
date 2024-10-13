ce17f51d312eb2ba15e6e4c61a6b19c489f40d2c
Sebastian Korfmann <sebastian@korfmann.net>
2024-10-13 23:48:21 +0200

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,9 @@ Rails.application.configure do
   config.cache_store = :solid_cache_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = Jumpstart.config.queue_adapter
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
+
   config.solid_queue.connects_to = {database: {writing: :queue}}
 
   # Ignore bad email addresses and do not raise email delivery errors.
